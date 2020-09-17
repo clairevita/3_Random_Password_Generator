cfece75ad14cc661cb7a8c367d8579818378e295
Claire Vita <clairej.vita@gmail.com>
2020-09-16 23:03:17 -0400

    Included a Fisher-Yates Shuffle to that randomizes the password. Enabled boolean checks and if statements in the random selection For loop to ensure that at least one of each character types selected is included in their final password. Updated the README to reflect this new change.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ When the page is opened the DOM API is used to connect to the textarea where the
 
 ### Button Press Initiation
 
-Once the HTML button is pressed, `finalPW` and `enabledChars` are opened as empty strings, acting as containers where the final password and selected character types will be stored. 
+Once the HTML button is pressed, `unscrambledPW` and `enabledChars` are opened as empty strings, acting as containers where the final password and selected character types will be stored. 
 
 `passPool` is set to equal `passChars.split("--")`, so each of the character types are distributed as indexes into an array. 
 
@@ -18,17 +18,47 @@ Once the HTML button is pressed, `finalPW` and `enabledChars` are opened as empt
 
 The first prompt, `passLength`, is requests the user specify their preffered password length. A check is made to ensure that their inputted value is a valid integer, not equal to zero and then inbetween 8 and 128. 
 
-Next, the confirm requests `askLower`, `askUpper`, `askNum`, and `askspecialChar` ask the user if they want lower case, upper case, number, and/or special characters. After each prompt, the respective `passPool[i]` will be added to the `enabledChars` container as a string.
+Next, the confirm requests `askLower`, `askUpper`, `askNum`, and `askspecialChar` ask the user if they want lower case, upper case, number, and/or special characters. After each prompt, the respective `passPool[i]` will be added to the `enabledChars` container as a string. When a character type is selected, a boolean variable is set to `True` defined for that particular character style.
 
 If the user selected no characters, they will be alerted for their mistake, then returned.
 
 ### Random Character Selection
 
-Once they've selected their character types, the follow for loop is called:
-```
-for (let i = 0; i < passLength; i++) { let random=Math.floor(Math.random() * enabledChars.length); finalPW +=enabledChars[random]; } 
-```
-Repeating for a length according to the user's `passLength`, the loop selects a number from `0` to 1 less than`enabledChars.length` as an index, defined as `random`. The loop adds a character from `enabledChars` from the `random` index slot, then adds it to the `finalPW` string.
+Once they've selected their character types, a `for` loop is opened. To ensure that this loop includes at least once character type of each of the selected, it checks boolean statuses defined by the prompts and if it has been added yet.
 
-After `passLength` loops, the  `finalPW` is returned to the user, populating the text area.
+If it has been ticked to be included and it has not been included yet, starting with lowercase characters, it uses the `passPool[i]` strings to randomly select a character. Once a character has been selected for particular bool-check, the added status is set to `True` for the following for loop pass. 
 
+This process is only enacted is the index is less than or equal to 3 and if `safetyCheck` is greater than 1, an integer counter ensuring that more than one character type was selected. If `safetyCheck` is equal to 1, than there is no concern for exclusion of a particular character type, and free randomized character selection can be done.
+
+The following script is our random character selection:
+
+```
+let random=Math.floor(Math.random() * enabledChars.length); unscrambledPW +=enabledChars[random]; 
+``` 
+
+
+### Jumbler
+
+At this point in the script, we have a string called `unscrambledPW`. This is considered an unsecure password, because it will always start with the between 2 and 4 of the character criteria they selected in the listed order of lowercase, uppercase, numerical, then sepcial character. 
+
+To ensure that the password is complete randomized with no consistent pattern, the script scrambles this previous string.
+
+To do this we split the string into an array using:
+```
+let jumbler = unscrambledPW.split("");
+```
+
+Then performs the following Fisher-Yates Shuffle:
+```
+for (let i = passLength-1; i > 0; i--){
+            //Starting with the last value in the jumbler array, this statement identifies the new position that a value in the array will be relocated to.
+            newPosition = Math.floor(Math.random() * (i + 1));
+            //The currently selected value in the for loop is assigned as a temporary value. 
+            temporaryValue = jumbler[i];
+            //It's replaced with the value from the original array that was randomly selected with the newPosition statement.
+            jumbler[i] = jumbler[newPosition];
+            //The newPosition location is replaced by the currently selected value in the for loop.
+            jumbler[newPosition] = temporaryValue;
+```
+
+After this Fisher-Yates Shuffle is completed, we join the array values in their new order, then return the user the secure password. 
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let passChars = "abcdefghijklmnopqrstuvwxyz--ABCDEFGHIJKLMNOPQRSTUVWXYZ--1234567890--!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\\";
+let passChars = "abcdefghijklmnopqrstuvwxyz--ABCDEFGHIJKLMNOPQRSTUVWXYZ--1234567890123456789--!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\\";
 
 
     // Write password to the #password input
@@ -18,7 +18,7 @@ let passChars = "abcdefghijklmnopqrstuvwxyz--ABCDEFGHIJKLMNOPQRSTUVWXYZ--1234567
     function generatePassword() {
 
     //This will be the final password returned at the end of the function.
-    let finalPW = "";
+    let unscrambledPW = "";
 
     //Creates the space where the enabled character types will be placed.
     let enabledChars = "";
@@ -42,22 +42,45 @@ let passChars = "abcdefghijklmnopqrstuvwxyz--ABCDEFGHIJKLMNOPQRSTUVWXYZ--1234567
         
     }
 
+    //This variable will be referenced to make sure at least one of each of the characters is being referenced in the final password.
+    let safetyCheck = 0;
+    //This establishes there should be at least 1 of each in the final password.
+    let lowerBool = false;
+    let upperBool = false;
+    let numBool = false;
+    let specialBool = false;
+
+    //These boolean variables will be referenced after adding at least 1 of each in the final password.
+    let lowerAdded = false;
+    let upperAdded = false;
+    let numAdded = false;
+    let specialAdded = false;
+
+
     //The following prompts ask the user for password content preferences and modifies the pool it will be randomly selecting from.
     let askLower = confirm("Would you like to include lower case characters?");
     if (askLower){
     enabledChars += passPool[0];
+    lowerBool = true;
+    safetyCheck ++;
     }
     let askUpper = confirm("Would you like to include upper case characters?");
     if (askUpper){
     enabledChars += passPool[1];
+    upperBool = true;
+    safetyCheck ++;
     }
     let askNum = confirm("Would you like to include numerical characters?");
     if (askNum){
     enabledChars += passPool[2];
+    numBool = true;
+    safetyCheck ++;
     }
     let askspecialChar = confirm("Would you like to include specials characters?");
     if (askspecialChar){
     enabledChars += passPool[3];
+    specialBool = true;
+    safetyCheck ++;
     }
 
     // If the user selected "No" for each of the character types, it alerts them to use a character type, then resets the function.
@@ -66,8 +89,64 @@ let passChars = "abcdefghijklmnopqrstuvwxyz--ABCDEFGHIJKLMNOPQRSTUVWXYZ--1234567
         return "";
     }
 
-    //According to how many characters long the user specified, repeatedly adds random characters from the pool defined by the prompts.
-    for (let i = 0; i < passLength; i++) { let random=Math.floor(Math.random() * enabledChars.length); finalPW +=enabledChars[random]; } 
-    //Returns the final password. 
-    return finalPW; }
+    for (let i = 0; i < passLength; i++) { 
+        // If more than one character type is selected, which is identified by the safety check, then a safety sequences will be prompted, ensuring at least one of each selected character type is added.
+        if(i<=3 && safetyCheck > 1){
+            //If they selected lowercase, and a lowercase character hasn't been added yet, select one at random and add it to the string.
+            if (lowerBool == true && lowerAdded == false){
+                let passLower = passPool[0];
+                let randomLower=Math.floor(Math.random() * passLower.length); unscrambledPW+= passLower[randomLower];
+                lowerAdded = true;
+            //If they selected uppercase, and an uppercase character hasn't been added yet, select one at random and add it to the string.
+            } else if (upperBool == true && upperAdded == false){
+                let passUpper = passPool[1];
+                let randomUpper=Math.floor(Math.random() * passUpper.length); unscrambledPW+=passUpper[randomUpper];
+                upperAdded = true;
+            //If they selected numbers, and a numerical character hasn't been added yet, select one at random and add it to the string.
+            } else if (numBool == true && numAdded == false){
+                let passNum = passPool[2];
+                let randomNum=Math.floor(Math.random() * passNum.length); unscrambledPW+=passNum[randomNum];
+                numAdded = true;
+            //If they selected special characters, and a special character hasn't been added yet, select one at random and add it to the string. 
+            } else if (specialBool == true && specialAdded == false){
+                let passSpecial = passPool[3];
+                let randomSpecial=Math.floor(Math.random() * passSpecial.length); unscrambledPW+=passSpecial[randomSpecial];
+            }
+            //Once this process has been enacted for a maximum of 4 times (once for each possible character type), the algorithm begins to select them randomly.
+        } else{
+            let random=Math.floor(Math.random() * enabledChars.length); unscrambledPW +=enabledChars[random]; 
+        } 
+    } 
+    
+        //This splits the unscrambled password into an array so that it can be randomized.
+        let jumbler = unscrambledPW.split("");
+
+        let newPosition;
+        let temporaryValue;
+
+        //This is referred to as a Fisher-Yates Shuffle. It will be scrambling our password. Otherwise if the user selects lowercase, the password will always start with a lowercase character. 
+        for (let i = passLength-1; i > 0; i--){
+            //Starting with the last value in the jumbler array, this statement identifies the new position that a value in the array will be relocated to.
+            newPosition = Math.floor(Math.random() * (i + 1));
+            //The currently selected value in the for loop is assigned as a temporary value. 
+            temporaryValue = jumbler[i];
+            //It's replaced with the value from the original array that was randomly selected with the newPosition statement.
+            jumbler[i] = jumbler[newPosition];
+            //The newPosition location is replaced by the currently selected value in the for loop.
+            jumbler[newPosition] = temporaryValue;    
+
+            //This process is repeated until all of the items have been relocated (at least) once. They may be swapped more than once. 
+
+        }
+        //Composes the scrambled array as the password string.
+        scrambledPW = jumbler.join('');
+        
+        //Returns the final password. 
+        return scrambledPW;
+    
+
+
+    }
+    
+     
     
